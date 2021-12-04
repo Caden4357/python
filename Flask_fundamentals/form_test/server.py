5f16c2b164f25c8409a36a753e94b00adf33afde
Caden Wilcox <wilcox4357@gmail.com>
2021-12-03 21:38:04 -0800

    playground

--- Flask_fundamentals/form_test/server.py
+++ Flask_fundamentals/form_test/server.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 # our index route will handle rendering our form
 @app.route('/')
 def index():
-  return render_template("index.html")
+    return render_template("index.html")
 # this route will handle our form submission
 # notice how we defined which HTTP methods are allowed by this route
 @app.route('/users', methods=['POST'])
